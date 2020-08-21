ac1b84292d13acaa10cac17d5fc569a603d9d71a
Кириченко Сергей <Sergey.Kirichenko@monitel.com>
2020-08-21 15:20:43 +0300

    chore(release-script): добавляет в скрипт создание релиза текущую ветку git

--- bin/release.js
+++ bin/release.js
@@ -2,17 +2,16 @@
 
 const {spawn} = require('child_process');
 const app = require('./my-app');
-
+const git = require('local-git-sync');
 
 
 // standard-version --release-as 11.6.2 --prerelease && git push --follow-tags origin master
-const sp = spawn('standard-version', ['--release-as', app.version, '--prerelease', '&&', 'git', 'push', '--follow-tags', 'origin', 'master'], {
+const sp = spawn('standard-version', ['--release-as', app.version, '--prerelease', '&&', 'git', 'push', '--follow-tags', 'origin', git.branch()], {
   stdio: ['inherit', 'inherit', 'inherit'],
   shell: true,
   // cwd: r.pathTo
 })
 
 sp.on('close', _ => {
-  console.log("run");
-  return
+  console.log(`Сборка ${version[0]}.${version[1]} готова`)
 })
